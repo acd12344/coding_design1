f312a9a527f3d4eb5470d46b37643d4d40fdc967
acd123 <2381278536@qq.com>
2022-10-16 15:47:43 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         /*11111111111111111111111111*/
         /*123123131231231*/
+        System.out.println("Hello world!");
+/**/
     }
 }
 
